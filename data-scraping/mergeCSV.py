aeac665c97edbd4cbecf8a8f2c5054ea9427266f
atmaja32 <atmajaraman32@gmail.com>
2021-10-25 23:32:25 -0400

    added a comment

--- data-scraping/mergeCSV.py
+++ data-scraping/mergeCSV.py
@@ -12,3 +12,5 @@ joined_list = glob.glob(joined_files)
 # Finally, the files are joined
 df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
 df.to_csv("spotify_music_consolidated.csv")
+
+#add a comment
